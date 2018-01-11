1b45ddce9dc3e7321d104c19aaefaa876065c652
wangzheng <wangzheng@guahao.com>
2018-01-11 17:51:42 +0800

    init python3 repo

---
+++ com/base/requests/Test.py
@@ -0,0 +1,12 @@
+import requests
+
+# r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+r = requests.get('https://www.baidu.com')
+print(r.status_code)
+print(r.headers['content-type'])
+print(r.encoding)
+print(r.text)
+print(r.cookies.__len__())
+d = r.cookies;
+for (d, x) in d.items():
+    print("key: " + d + ", value: " + x)
